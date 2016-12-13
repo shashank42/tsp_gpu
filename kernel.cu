ebc99762e3b10a4d12d0739556b1f7ec067f7b8e
Haoyan <hm2689@columbia.edu>
2016-12-13 02:40:27 -0500

    Perfect one
    No edge problem, no increase

---
+++ kernel.cu
@@ -0,0 +1,377 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+#include <cuda.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+#include <assert.h>
+#include <iostream> 
+#include <fstream>
+
+
+#include "utils.h"
+#include "tsp_solve.h"
+
+
+#define t_num 1024
+#define GRID_SIZE 9216
+
+/*
+For more samples define GRID_SIZE as a multiple of t_num such as 512000, 2048000, or the (max - 1024) grid size 2147482623
+Some compliation options that can speed things up
+--use_fast_math
+--optimize=5
+--gpu-architecture=compute_35
+I use something like
+NOTE: You need to use the -lcurand flag to compile.
+nvcc --optimize=5 --use_fast_math -arch=compute_35 kernel.cu -o tsp_cuda -lcurand
+*/
+
+int main(){
+
+	const char *tsp_name = "dsj1000.tsp";
+	read_tsp(tsp_name);
+	unsigned int N = meta->dim, *N_g;
+	// start counters for cities 
+	unsigned int i;
+
+	coordinates *location_g;
+
+	/* For checking the coordinates
+	for (i = 0; i < N; i++)
+	printf("Location x: %0.6f, location y: %0.6f \n", location[i].x, location[i].y);
+	exit(0);
+	*/
+	unsigned int *salesman_route = (unsigned int *)malloc((N + 1) * sizeof(unsigned int));
+
+	// just make one inital guess route, a simple linear path
+	for (i = 0; i <= N; i++)
+		salesman_route[i] = i;
+
+	// Set the starting and end points to be the same
+	salesman_route[N] = salesman_route[0];
+
+	/*     don't need it when importing data from files
+	// initialize the coordinates and sequence
+	for(i = 0; i < N; i++){
+	location[i].x = rand() % 1000;
+	location[i].y = rand() % 1000;
+	}
+	*/
+
+	/*
+	FILE *fp;
+	fp = fopen("sra_best_trip.csv", "r");   //open file,read only
+	i = 1;
+	int j;
+	fscanf(fp, "%d", &i);
+	while (i != 0)           //keep moving the pointer fp until it points to index 1
+	{
+		fseek(fp, 1, SEEK_CUR);
+		fscanf(fp, "%d", &i);
+	}
+	printf("%d\n", i);
+	for (j = 1; j <= 104814; j++)
+	{
+
+		fseek(fp, 26, SEEK_CUR);
+		fscanf(fp, "%d", &i);
+		printf("%d\n", i);
+		salesman_route[j] = i;
+	}
+	*/
+
+
+	// Calculate the original loss 
+	float original_loss = 0;
+	for (i = 0; i < N; i++){
+		original_loss += sqrtf((location[salesman_route[i]].x - location[salesman_route[i + 1]].x) *
+			(location[salesman_route[i]].x - location[salesman_route[i + 1]].x) +
+			(location[salesman_route[i]].y - location[salesman_route[i + 1]].y) *
+			(location[salesman_route[i]].y - location[salesman_route[i + 1]].y));
+	}
+	printf("Original Loss is:  %0.6f \n", original_loss);
+	// Keep the original loss for comparison pre/post algorithm
+	// SET THE LOSS HERE
+	float T[1], *T_g;
+	T[0] = 0.3; 
+	/*
+	Defining device variables:
+	city_swap_one_h/g: [integer(t_num)]
+	- Host/Device memory for city one
+	city_swap_two_h/g: [integer(t_num)]
+	- Host/Device memory for city two
+	flag_h/g: [integer(t_num)]
+	- Host/Device memory for flag of accepted step
+	salesman_route_g: [integer(N)]
+	- Device memory for the salesmans route
+	flag_h/g: [integer(t_num)]
+	- host/device memory for acceptance vector
+	original_loss_g: [integer(1)]
+	- The device memory for the current loss function
+	(DEPRECATED)new_loss_h/g: [integer(t_num)]
+	- The host/device memory for the proposal loss function
+	*/
+	unsigned int *city_swap_one_h = (unsigned int *)malloc(GRID_SIZE * sizeof(unsigned int));
+	unsigned int *city_swap_two_h = (unsigned int *)malloc(GRID_SIZE * sizeof(unsigned int));
+	unsigned int *flag_h = (unsigned int *)malloc(GRID_SIZE * sizeof(unsigned int));
+	unsigned int *salesman_route_g, *salesman_route_2g, *flag_g, *city_swap_one_g, *city_swap_two_g;
+	unsigned int global_flag_h = 0, *global_flag_g_1, *global_flag_g_2, *global_flag_g_3;
+	unsigned int *global_flag_g_4, *global_flag_g_5;
+
+	cudaMalloc((void**)&city_swap_one_g, GRID_SIZE * sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&city_swap_two_g, GRID_SIZE * sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&location_g, N * sizeof(coordinates)); 
+	cudaCheckError();
+	cudaMalloc((void**)&salesman_route_g, (N + 1) * sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&salesman_route_2g, (N + 1) * sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&T_g, sizeof(float));
+	cudaCheckError();
+	cudaMalloc((void**)&flag_g, GRID_SIZE * sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&global_flag_g_1, sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&global_flag_g_2, sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&global_flag_g_3, sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&global_flag_g_4, sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&global_flag_g_5, sizeof(unsigned int));
+	cudaCheckError();
+	cudaMalloc((void**)&N_g, sizeof(unsigned int));
+	cudaCheckError();
+
+
+	cudaMemcpy(location_g, location, N * sizeof(coordinates), cudaMemcpyHostToDevice);
+	cudaCheckError();
+	cudaMemcpy(salesman_route_g, salesman_route, (N + 1) * sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaCheckError();
+	cudaMemcpy(salesman_route_2g, salesman_route, (N + 1) * sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaCheckError();
+	cudaMemcpy(global_flag_g_1, &global_flag_h, sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaCheckError();
+	cudaMemcpy(global_flag_g_2, &global_flag_h, sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaCheckError();
+	cudaMemcpy(global_flag_g_3, &global_flag_h, sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaCheckError();
+	cudaMemcpy(global_flag_g_4, &global_flag_h, sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaCheckError();
+	cudaMemcpy(global_flag_g_5, &global_flag_h, sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaCheckError();
+	cudaMemcpy(N_g, &N, sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaCheckError();
+	// Beta is the decay rate
+	//float beta = 0.0001;
+	// We are going to try some stuff for temp from this adaptive simulated annealing paper
+	// https://arxiv.org/pdf/cs/0001018.pdf
+
+	// Number of thread blocks in grid
+	// X is for the sampling, y is for manipulating the salesman's route
+	dim3 blocksPerSampleGrid(GRID_SIZE / t_num, 1, 1);
+	dim3 blocksPerTripGrid((N / t_num) + 1, 1, 1);
+	dim3 threadsPerBlock(t_num, 1, 1);
+
+	// Trying out random gen in cuda
+	curandState_t* states;
+
+	/* allocate space on the GPU for the random states */
+	cudaMalloc((void**)&states, GRID_SIZE * sizeof(curandState_t));
+	init << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(time(0), states);
+
+	//time counter
+	time_t t_start, t_end;
+	t_start = time(NULL); 
+
+	while (T[0] > 0.01 / log(20 * N))
+	{
+		// Copy memory from host to device
+		cudaMemcpy(T_g, T, sizeof(float), cudaMemcpyHostToDevice);
+		i = 1;              
+		 
+		while (i<2000){                                                                                         // key
+
+			cudaError_t e = cudaGetLastError();
+			if (e != cudaSuccess) {
+				printf(" Temperature was %.6f on failure\n", T[0]);
+			} 
+			tspSwap_f << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g,
+				T_g, global_flag_g_1, N_g,
+				states); 
+			cudaCheckError();
+			tspSwapUpdate << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, global_flag_g_1);
+			cudaCheckError();
+			tspSwap_b << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g,
+				T_g, global_flag_g_2, N_g,
+				states);
+			cudaCheckError();
+			tspSwapUpdate << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, global_flag_g_2); 
+			cudaCheckError(); 
+			
+			
+			tsp_2_Opt << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g, 
+				T_g, global_flag_g_3, N_g,
+				states);
+			cudaCheckError(); 
+			tspInsertionUpdateTrip << <blocksPerTripGrid, threadsPerBlock, 0 >> >(salesman_route_g, salesman_route_2g, N_g);
+			cudaCheckError();
+			tsp_2_Opt_Update << <blocksPerTripGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, salesman_route_2g, global_flag_g_3);
+			cudaCheckError();
+			tsp_2_Opt << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g,
+				T_g, global_flag_g_3, N_g,
+				states);
+			cudaCheckError();
+			tspInsertionUpdateTrip << <blocksPerTripGrid, threadsPerBlock, 0 >> >(salesman_route_g, salesman_route_2g, N_g);
+			cudaCheckError();
+			tsp_2_Opt_Update << <blocksPerTripGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, salesman_route_2g, global_flag_g_3);
+			cudaCheckError();
+
+	/*		tsp_2_Opt << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g,
+				T_g, global_flag_g, N_g,
+				states);
+			cudaCheckError();
+			tspInsertionUpdateTrip << <blocksPerTripGrid, threadsPerBlock, 0 >> >(salesman_route_g, salesman_route_2g, N_g);
+			cudaCheckError();
+			tsp_2_Opt_Update << <blocksPerTripGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, salesman_route_2g, global_flag_g);
+			cudaCheckError();
+			tsp_2_Opt << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g,
+				T_g, global_flag_g, N_g,
+				states);
+			cudaCheckError();
+			tspInsertionUpdateTrip << <blocksPerTripGrid, threadsPerBlock, 0 >> >(salesman_route_g, salesman_route_2g, N_g);
+			cudaCheckError();
+			tsp_2_Opt_Update << <blocksPerTripGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, salesman_route_2g, global_flag_g);
+			cudaCheckError();
+			tsp_2_Opt << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g,
+				T_g, global_flag_g, N_g,
+				states);
+			cudaCheckError();
+			tspInsertionUpdateTrip << <blocksPerTripGrid, threadsPerBlock, 0 >> >(salesman_route_g, salesman_route_2g, N_g);
+			cudaCheckError();
+			tsp_2_Opt_Update << <blocksPerTripGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, salesman_route_2g, global_flag_g);
+			cudaCheckError();  */
+			
+	/*		tspInsertion2 << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g,
+				T_g, global_flag_g, N_g,
+				states); 
+			cudaCheckError();
+			tspInsertionUpdateTrip << <blocksPerTripGrid, threadsPerBlock, 0 >> >(salesman_route_g, salesman_route_2g, N_g);
+			cudaCheckError();
+			tspInsertionUpdate2 << <blocksPerTripGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, salesman_route_2g, global_flag_g);
+			cudaCheckError();
+			*/
+			
+			tspInsertion_b << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g,
+				T_g, global_flag_g_4, N_g,
+				states);
+			cudaCheckError();
+			tspInsertionUpdateTrip << <blocksPerTripGrid, threadsPerBlock, 0 >> >(salesman_route_g, salesman_route_2g, N_g);
+			cudaCheckError();
+			tspInsertionUpdate2 << <blocksPerTripGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, salesman_route_2g, global_flag_g_4);
+			cudaCheckError(); 
+			tspInsertion_f << <blocksPerSampleGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				location_g, salesman_route_g,
+				T_g, global_flag_g_5, N_g,
+				states);
+			cudaCheckError();
+			tspInsertionUpdateTrip << <blocksPerTripGrid, threadsPerBlock, 0 >> >(salesman_route_g, salesman_route_2g, N_g);
+			cudaCheckError();
+			tspInsertionUpdate2 << <blocksPerTripGrid, threadsPerBlock, 0 >> >(city_swap_one_g, city_swap_two_g,
+				salesman_route_g, salesman_route_2g, global_flag_g_5);
+			cudaCheckError();
+			
+			//		iter += 1.00f;
+			//		T = T_start / log(iter);
+			//		if ((long int)iter % 50000 == 0)
+			//			printf("Iter: %ld  Temperature is %.6f\n", (long int)iter, T);
+			//T = 1;
+			i++;
+		}
+		cudaMemcpy(salesman_route, salesman_route_g, (N + 1) * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+		cudaCheckError();
+		float optimized_loss = 0;
+		for (i = 0; i < N; i++){
+			optimized_loss += sqrt((location[salesman_route[i]].x - location[salesman_route[i + 1]].x) *
+				(location[salesman_route[i]].x - location[salesman_route[i + 1]].x) +
+				(location[salesman_route[i]].y - location[salesman_route[i + 1]].y) *
+				(location[salesman_route[i]].y - location[salesman_route[i + 1]].y));
+		}
+		printf("Optimized Loss is: %.6f \n", optimized_loss);
+		T[0] = T[0] * 0.99;
+		printf("T[0] %f  \n", T[0]);
+	}
+	//print time spent 
+	t_end = time(NULL);
+	printf("time = %f\n", difftime(t_end, t_start));
+
+	cudaMemcpy(salesman_route, salesman_route_g, (N + 1) * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+	cudaCheckError();
+	float optimized_loss = 0;
+	for (i = 0; i < N; i++){
+		optimized_loss += sqrt((location[salesman_route[i]].x - location[salesman_route[i + 1]].x) *
+			(location[salesman_route[i]].x - location[salesman_route[i + 1]].x) +
+			(location[salesman_route[i]].y - location[salesman_route[i + 1]].y) *
+			(location[salesman_route[i]].y - location[salesman_route[i + 1]].y));
+	}
+	printf("Optimized Loss is: %.6f \n", optimized_loss);
+
+	// Write the best trip to CSV
+	FILE *best_trip;
+	const char *filename = "mona_lisa_best_trip.csv";
+	best_trip = fopen(filename, "w+");
+	fprintf(best_trip, "location,coordinate_x,coordinate_y\n");
+	for (i = 0; i < N + 1; i++){
+		fprintf(best_trip, "%d,%.6f,%.6f\n",
+			salesman_route[i],
+			location[salesman_route[i]].x,
+			location[salesman_route[i]].y);
+	}
+	fclose(best_trip);
+
+	/*
+	printf("\n Final Route:\n");
+	for (i = 0; i < N; i++)
+	printf("%d ",salesman_route[i]);
+	*/
+	cudaFree(location_g);
+	cudaCheckError();
+	cudaFree(salesman_route_g);
+	cudaCheckError();
+	cudaFree(salesman_route_2g);
+	cudaCheckError();
+	cudaFree(T_g);
+	cudaCheckError();
+	cudaFree(flag_g);
+	cudaCheckError();
+	free(salesman_route);
+	free(city_swap_one_h);
+	free(city_swap_two_h);
+	free(flag_h);
+	free(location);
+	getchar();
+	return 0;
+}
+
---
+++ utils.h
@@ -0,0 +1,211 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+ 
+
+ 
+#define LINE_BUF_LEN 100
+
+/********************************************************
+* Function to check for cuda error
+*  Run this on the host after performing a cuda operation.
+*****************************************************/
+#define cudaCheckError() {                                          \
+ cudaError_t e=cudaGetLastError();                                 \
+ if(e!=cudaSuccess) {                                              \
+   printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));\
+   exit(0); \
+ }                                                                 \
+}
+
+/*****************************
+* Desc: structs for coordinates and tsp meta data
+* 
+* coordinates is the  CODE_COORD_SECTION of the tsp file
+* 
+* meta_data contains the information
+* NAME:
+* COMMENT:
+* DIMENSION:
+* EDGE_WEIGHT_TYPE:
+******************************/
+ typedef struct coordinates {
+    int id;
+    float x,y;
+} coordinates;
+
+
+typedef struct meta_data {
+    char *name; char *comment;
+    int dim; char *edge_type; 
+} MetaTSPData;
+
+coordinates *location;
+MetaTSPData *meta = (MetaTSPData *)malloc(sizeof(MetaTSPData)); 
+
+/*****************************
+* Desc: Reads in files that are in the tsp format
+*
+* Input: 
+* - tsp_file_name [const char()]
+*  - The name of the tsp file to retrieve data from
+* 
+* Based on: https://github.com/bishma-stornelli/CVRP-AOC/blob/master/TSP-TEST.V0.9/instance.c
+*******************************/
+void read_tsp(const char *tsp_file_name){
+    FILE * tsp_file;
+    char buf[LINE_BUF_LEN];
+    char name[LINE_BUF_LEN];
+    char EDGE_WEIGHT_TYPE[LINE_BUF_LEN];
+    char comment[LINE_BUF_LEN];
+    char  dim[LINE_BUF_LEN];
+          	  
+    
+    
+     
+    
+    tsp_file = fopen(tsp_file_name, "r");
+    if (tsp_file == NULL)
+        exit(EXIT_FAILURE);
+    if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed at first line\n");
+    while ( strcmp("NODE_COORD_SECTION", buf) != 0 ) {
+    //printf("%s \n",buf);
+	if ( strcmp("NAME", buf) == 0 ) {
+	    if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed at Name\n");
+	    // TRACE ( printf("%s ", buf); )
+	    if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed at Name\n");
+	    strcpy(name, buf);
+	    meta -> name = strdup(name);
+	    //printf("%s \n",meta->name);
+	    buf[0]=0;
+	}
+	else if ( strcmp("NAME:", buf) == 0 ) {
+	    if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed at Name:\n");
+	    strcpy(name, buf);
+	    meta -> name = strdup(name);
+	    //printf("%s \n",meta->name);
+	    buf[0]=0;
+	}
+	else if ( strcmp("COMMENT", buf) == 0 ){
+	    if(!fgets(buf, LINE_BUF_LEN, tsp_file)) printf("Reading failed at Comment\n");
+	    strcpy(comment, buf);
+	    meta -> comment = strdup(comment);
+	    buf[0]=0;
+	}
+	else if ( strcmp("COMMENT:", buf) == 0 ){
+	    if(!fgets(buf, LINE_BUF_LEN, tsp_file)) printf("Reading failed at Comment:\n");
+        strcpy(comment, buf);
+	    meta -> comment = strdup(comment);
+	    buf[0]=0;
+	}
+	else if ( strcmp("TYPE", buf) == 0 ) {
+	    if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed at Type\n");
+	    // TRACE ( printf("%s ", buf); )
+	    if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed at Type\n");
+	    // TRACE ( printf("%s\n", buf); )
+	    if( strcmp("TSP", buf) != 0 ) {
+		fprintf(stderr,"\n Not a TSP instance in TSPLIB format !!\n");
+		exit(1);
+	    }
+	    buf[0]=0;
+	}
+	else if ( strcmp("TYPE:", buf) == 0 ) {
+	    if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed at Name:\n");
+	    // TRACE ( printf("%s\n", buf); )
+	    if( strcmp("TSP", buf) != 0 ) {
+		fprintf(stderr,"\n Not a TSP instance in TSPLIB format !!\n");
+		exit(1);
+	    }
+	    buf[0]=0;
+	}
+	else if( strcmp("DIMENSION", buf) == 0 ){
+	    if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed at Dim\n");
+	    strcpy(dim, buf);
+	    meta -> dim = atoi(strdup(dim));
+	    buf[0]=0;
+	}
+	else if ( strcmp("DIMENSION:", buf) == 0 ) {
+	    if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed at Dim:\n");
+	    strcpy(dim, buf);
+	    meta -> dim = atoi(strdup(dim));
+	    buf[0]=0;
+	}
+	else if( strcmp("DISPLAY_DATA_TYPE", buf) == 0 ){
+	    if(!fgets(buf, LINE_BUF_LEN, tsp_file)) printf("Reading failed at Display Data Type\n");
+	    // TRACE ( printf("%s", buf); );
+	    buf[0]=0;
+	}
+	else if ( strcmp("DISPLAY_DATA_TYPE:", buf) == 0 ) {
+	    if(!fgets(buf, LINE_BUF_LEN, tsp_file)) printf("Reading failed at Display Data Type\n");
+	    // TRACE ( printf("%s", buf); );
+	    buf[0]=0;
+	}
+	else if( strcmp("EDGE_WEIGHT_TYPE:", buf) == 0 ){
+	    if(!fgets(buf, LINE_BUF_LEN, tsp_file)) printf("Reading failed at Edge Weight Type\n");
+	    strcpy(EDGE_WEIGHT_TYPE, buf);
+	    meta -> edge_type = strdup(EDGE_WEIGHT_TYPE);
+	    buf[0]=0;
+	}
+	buf[0]=0;
+	if (fscanf(tsp_file,"%s", buf) <= 0) printf("Reading file failed during read\n");
+    }
+    printf("edge_type: %s", meta->edge_type);
+    printf("Dimension: %d \n",meta-> dim);
+
+    //Set the location structs size.
+    location = (coordinates *)malloc(meta->dim * sizeof(coordinates));
+    for (int i = 0 ; i < meta->dim ; i++ ) {
+	    if(fscanf(tsp_file,"%d %f %f", &location[i].id, &location[i].x, &location[i].y) <=0)
+	      printf("Reading failed while scanning coordinates\n");
+    }
+    fclose(tsp_file);
+}
+// END READ TSP    
+
+ /* Function to generate random numbers in interval
+ 
+ input:
+- min [unsigned integer(1)]
+  - The minimum number to sample
+- max [unsigned integer(1)]
+  - The maximum number to sample
+  
+  Output: [unsigned integer(1)]
+    - A randomly generated number between the range of min and max
+    
+  Desc:
+  Taken from
+  - http://stackoverflow.com/questions/2509679/how-to-generate-a-random-number-from-within-a-range
+  
+  
+ */
+ unsigned int rand_interval(unsigned int min, unsigned int max)
+{
+    int r;
+    const unsigned int range = 1 + max - min;
+    const unsigned int buckets = RAND_MAX / range;
+    const unsigned int limit = buckets * range;
+
+    /* Create equal size buckets all in a row, then fire randomly towards
+     * the buckets until you land in one of them. All buckets are equally
+     * likely. If you land off the end of the line of buckets, try again. */
+    do
+    {
+        r = rand();
+    } while (r >= limit);
+
+    return min + (r / buckets);
+}
+
+/*****************************
+* Desc: Reads in files that are in the tsp format
+*
+* Input: 
+* - tsp_file_name [const char()]
+*  - The name of the tsp file to retrieve data from
+* 
+* Based on: https://github.com/bishma-stornelli/CVRP-AOC/blob/master/TSP-TEST.V0.9/instance.c
+*******************************/
+
+
+
+#endif //_UTILS_H_
